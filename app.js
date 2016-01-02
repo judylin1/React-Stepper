9154206b1959cec3a1ba77b8d04b8f10b8e26b2e
JudyL <judith.s.lin@gmail.com>
2016-01-02 14:48:02 -0600

    fix

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 import React, { Component, PropTypes } from 'react'
 import ReactDOM from 'react-dom'
-import { Multistep } from '../../src/index'
+import { Multistep } from './src/index'
 import { steps } from './js/signup/index.js'
 
 class App extends React.Component {
