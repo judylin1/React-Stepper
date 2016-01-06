f8a5410bddde3a2416faf1598b824c4649ba27c6
JudyL <judith.s.lin@gmail.com>
2016-01-06 11:27:01 -0600

    linting fixes

--- js/signup/index.js
+++ js/signup/index.js
@@ -4,10 +4,10 @@ import { SelectSuppliers } from './select-suppliers';
 import { ReviewCreate } from './review-create';
 
 const steps =
-    [
-      {name: 'Contract Settings', component: <ContractSettings/>},
-      {name: 'Select Suppliers', component: <SelectSuppliers/>},
-      {name: 'Review & Create', component: <ReviewCreate/>},
-    ]
+  [
+    {name: 'Contract Settings', component: <ContractSettings/>},
+    {name: 'Select Suppliers', component: <SelectSuppliers/>},
+    {name: 'Review & Create', component: <ReviewCreate/>},
+  ]
 
 export { steps }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@
 import React, { Component, PropTypes } from 'react';
 
 function getNavStates(index, length) {
-  let styles = []
-  for (let i=0; i<length; i++) {
-    (i < index) ? styles.push('done') : (i === index) ? styles.push('doing') : styles.push('todo')
+  const styles = [];
+  for (let i = 0; i < length; i ++) {
+    if (i < index) {
+      styles.push('done');
+    } else if (i === index) {
+      styles.push('doing');
+    } else {
+      styles.push('todo');
+    }
   }
-  return { current: index, styles: styles }
+  return { current: index, styles };
 }
 
 const Multistep = React.createClass({
@@ -15,58 +21,57 @@ const Multistep = React.createClass({
     return {
       compState: 0,
       navState: getNavStates(0, this.props.steps.length),
-    }
+    };
   },
 
   setNavState(next) {
-    this.setState({navState: getNavStates(next, this.props.steps.length)})
-    if(next < this.props.steps.length) {
-      this.setState({compState: next})
+    this.setState({ navState: getNavStates(next, this.props.steps.length) })
+    if (next < this.props.steps.length) {
+      this.setState({ compState: next });
     }
   },
 
   // if enter key (13) is pressed
   handleKeyDown(e) {
-    if(e.which === 13) {
-      this.next()
+    if (e.which === 13) {
+      this.next();
     }
   },
 
   handleOnClick(e) {
-    if(e.target.value  === (this.props.steps.length-1) &&
-       this.state.compState === (this.props.steps.length-1))     {
+    if (e.target.value === (this.props.steps.length-1) &&
+      this.state.compState === (this.props.steps.length-1)) {
       this.setNavState(this.props.steps.length)
-    }
-    else {
-      this.setNavState(e.target.value)
+    } else {
+      this.setNavState(e.target.value);
     }
   },
 
   next() {
-    this.setNavState(this.state.compState + 1)
+    this.setNavState(this.state.compState + 1);
   },
 
   previous() {
     if (this.state.compState > 0) {
-      this.setNavState(this.state.compState - 1)
+      this.setNavState(this.state.compState - 1);
     }
   },
 
-  render: function() {
-    const _this = this
+  render() {
+    const _this = this;
 
     return (
-      <div className='stepper-container' onKeyDown={this.handleKeyDown}>
+      <div className="stepper-container" onKeyDown={this.handleKeyDown}>
         {this.props.steps.map(function (s, i) {
           return (
-            <div className='step-container'>
-              <li value={i} key={i} className={"stepper-container-" + _this.state.navState.styles[i]} onClick={_this.handleOnClick}>
-                <span className='step-title'>{_this.props.steps[i].name}</span>
-                <span className='step-grey-line'></span>
+            <div className="step-container">
+              <li value={i} key={i} className={'stepper-container-' + _this.state.navState.styles[i]} onClick={_this.handleOnClick}>
+                <span className="step-title">{_this.props.steps[i].name}</span>
+                <span className="step-grey-line"></span>
               </li>
             </div>
-          )
-      }, this)}
+          );
+        }, this)}
         {this.props.steps[this.state.compState].component}
       </div>
     );
